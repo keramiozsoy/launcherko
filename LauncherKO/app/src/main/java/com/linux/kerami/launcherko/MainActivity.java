8413f812713d73ecb56c5c2da90c6d2103f2b267
Kerami Ozsoy <keramiozsoy@gmail.com>
2016-11-26 16:28:27 +0300

    LauncherKO project is created

---
+++ LauncherKO/app/src/main/java/com/linux/kerami/launcherko/MainActivity.java
@@ -0,0 +1,13 @@
+package com.linux.kerami.launcherko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
